e2fcff7ab130c1e625f120b5b09b296b8c4034b3
Frank van Mourik <fgvanmourik@gmail.com>
2018-04-12 15:39:22 +0200

    Category and Transactions test added

--- src/test/nl/utwente/ing/team/e/testing/BaseTest.java
+++ src/test/nl/utwente/ing/team/e/testing/BaseTest.java
@@ -1,5 +1,6 @@
 package nl.utwente.ing.team.e.testing;
 
+import org.json.JSONObject;
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.when;
@@ -9,27 +10,41 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class BaseTest {
 
+    private static final String SESSIONS = "/SESSIONS";
+    private static final String TRANSACTIONS = "/TRANSACTIONS";
+    private static final String TRANSACTIONS_ID = "/TRANSACTIONS/{id}";
+    private static final String TRANSACTIONS_ID_CATEGORY = "/TRANSACTIONS/{id}/category";
+    private static final String CATEGORIES = "/categories";
+    private static final String CATEGORIES_ID = "/categories/{id}";
 
-    private static final String[][] endPoints = new String[][]{
-            //Here should come all the endpooints of the api
-    };
+    //Here should come all the endpooints of the api
+
     //SESSIONS
 
+    @Test
     public void getSession() {
         when().
-                post("/api/v1/sessions").
+                post(SESSIONS).
                 then().assertThat().
-                statusCode(200).
+                statusCode(201).
                 contentType("application/json").
-                body(null); //Still needs a body
+                body("id", equalTo(313)); //Still needs a body
+    }
+
+    @Test
+    public void transcationsTest() {
+        given().
+                parameters("offset", 0, "limit", 0, "category", "category-name").
+                when().
+                get(TRANSACTIONS).
+                then().
+                body("category.name", equalTo("category-name"));
     }
 
     @Test
     public void transactionIdTest() {
-
-
         when().
-                get("/transactions/{id}", 0).
+                get(TRANSACTIONS_ID, 0).
                 then().
                 statusCode(200).
                 body("id", equalTo(0),
@@ -39,21 +54,69 @@ public class BaseTest {
                         "type:", equalTo("deposit"),
                         "category.id", equalTo(0),
                         "category.name", equalTo("string"));
+
+        when().
+                get(TRANSACTIONS_ID, "Some String").
+                then().
+                statusCode(400);
+
+        when().
+                get(TRANSACTIONS_ID, Double.POSITIVE_INFINITY).
+                then().
+                statusCode(404);
     }
 
     @Test
+    public void categoryTest(){
+        given().
+                parameters("offset", 0, "limit", 0, "category", "category-name").
+                when().
+                get(TRANSACTIONS).
+                then().
+                body("category.name", equalTo("category-name"));
+    }
+    @Test
     public void categoryIdTest() {
+        JSONObject CATEGORY_PUT = new JSONObject();
+        CATEGORY_PUT.append("name", "string");
+//        JSONObject CATEGORY_PUT_BODY = new JSONObject();
+//        CATEGORY_PUT_BODY.append("id", 25);
+//        CATEGORY_PUT_BODY.append("name", "groceries");
         when().
-                get("/categories/{id", 0).
+                get(CATEGORIES_ID, 0).
                 then().
                 statusCode(200).
                 body("id", equalTo(0),
                         "name", equalTo("string"));
+        when().
+                get(CATEGORIES_ID, "Some String").
+                then().
+                statusCode(400);
+
+        when().
+                get(CATEGORIES_ID, Double.POSITIVE_INFINITY).
+                then().
+                statusCode(404);
+
+        given().
+                parameters("categoryId", 25, "body", CATEGORY_PUT).
+                when().
+                put(CATEGORIES_ID).
+                then().
+                body("id", equalTo(25), "name", equalTo("groceries"));
+
+        when().
+                delete(CATEGORIES_ID, Double.POSITIVE_INFINITY).
+                then().
+                statusCode(404);
+
+        when().
+                delete(CATEGORIES_ID, 0).
+                then().
+                statusCode(204);
+
     }
 
-    @Test
-    public void simpleTest() {
 
-    }
 
 }
